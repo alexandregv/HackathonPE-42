789258a361d3bfb9f9a5725c89e64e9e29942e40
triinoxys <contact@triinoxys.fr>
2019-05-11 11:41:20 +0200

    Add devise candidat

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
   before_action :authenticate_entreprise!, only: [:recruter]
+  before_action :authenticate_candidat!, only: [:candidater]
 
   def index
   end
 
   def recruter
+  end
+
+  def candidater
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :entreprises, path: 'entreprises', controllers: { sessions: 'entreprises/sessions' }
+  devise_for :candidats,   path: 'candidats',   controllers: { sessions: 'candidats/sessions' }
 
   get '/',            to: 'pages#index',      as: 'index'
   get '/aide',        to: 'pages#aide',       as: 'aide'
   get '/recruter',    to: 'pages#recruter',   as: 'recruter'
+  get '/candidater',  to: 'pages#candidater', as: 'candidater'
 
   root to: 'pages#index'
 end
